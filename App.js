a00ae736d85ce7961b387a919a32e72171bd7074
reza 16t <swary2021@gmail.com>
2024-04-12 18:19:44 +0330

    start

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { NativeWindStyleSheet } from "nativewind";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+NativeWindStyleSheet.setOutput({
+   default: "native",
+});
+
+const queryClient = new QueryClient();
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+   return (
+      <QueryClientProvider client={queryClient}>
+         <SafeAreaView>
+            <Text>Open up App.js to start working on your app!</Text>
+            <StatusBar style="auto" />
+         </SafeAreaView>
+      </QueryClientProvider>
+   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+   container: {
+      flex: 1,
+      backgroundColor: "#fff",
+      alignItems: "center",
+      justifyContent: "center",
+   },
 });
