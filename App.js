ae66c83309adea5c77b447c9dd10034fa0669df9
reza 16t <swary2021@gmail.com>
2024-04-21 02:29:19 +0330

    add

--- App.js
+++ App.js
@@ -1,5 +1,3 @@
-// import { } from "expo-status-bar";
-import "react-native-gesture-handler";
 import { SafeAreaView, StatusBar } from "react-native";
 import { NativeWindStyleSheet } from "nativewind";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
